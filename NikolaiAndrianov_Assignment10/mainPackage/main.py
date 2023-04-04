293e5c1292307316e8c66aadcb7ce88208ec9369
nclow <nclow@Spectre.localdomain>
2023-04-03 20:15:06 -0400

    Test Commit

---
+++ NikolaiAndrianov_Assignment10/mainPackage/main.py
@@ -0,0 +1,8 @@
+#Name: Nikolai Andrianov - 
+#email: lowenc@mail.uc.edu
+#Assignment Title: Assignment 0
+#Course: IS 4010
+#Semester/Year: Spring 2023
+#Brief Description: 
+#Citations: 
+#Anything else that's relevant:
